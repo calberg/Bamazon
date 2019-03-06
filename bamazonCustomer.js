a8b0a2e29f703ddee9aaef9616266bc3810f4791
Calberg89 <chris_alberg@yahoo.com>
2019-03-06 12:49:17 -0800

    updated app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+availableproducts =[];
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -17,32 +18,92 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
   showProducts();
-  runSearch();
 });
 
 function showProducts() {
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products WHERE stock_quantity>0;", function(err, res) {
     for (var i = 0; i < res.length; i++) {
-      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].dept_name + " | " + res[i].price + " | " + res[i].stock_quantity);
-    }
-    console.log("-----------------------------------");
+      availableproducts.push(res[i].product_name);
+      console.log(i+1 + ".) " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].dept_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+    };
+    promptQuestion();
   });
+};
+
+function promptQuestion() {
+  inquirer.prompt({
+        message: "Which product would you like to buy?",
+        type: "list",
+        name: "nameofProduct",
+        choices: availableproducts
+    }).then(checkStock)
+  }
+    
+function checkStock(answer){
+    var productname = answer.nameofProduct;
+    var query = "SELECT stock_quantity, price FROM products WHERE ?"
+    connection.query(query, {product_name: answer.nameofProduct}, function(err, res) {
+      if (err) throw err;
+      var price = res[0].price;
+      if (res[0].stock_quantity >0) {
+        promptUserAmount(res[0].stock_quantity, price, productname);
+      }
+    });
+  }
+function promptUserAmount(itemamount, price, productname){
+  inquirer.prompt({
+    message: "How many would you like to buy?",
+    type: "input",
+    name: "amountPurchase"
+  }).then(function (action) {handleAmount(action, itemamount, price, productname);});
+  }
+
+function handleAmount(answer, itemamount, price, productname){
+  console.log("Price: " + price);
+  if (answer.amountPurchase > itemamount) {
+    console.log("sorry, we don't have enough of that item. We have " + itemamount + " of that item.");
+    promptUserAmount(productname);
+  }
+  if (answer.amountPurchase <= itemamount) {
+    var totalPurchaseCost = price * answer.amountPurchase
+    var newQuantity = itemamount - answer.amountPurchase
+    console.log("Total cost: " + totalPurchaseCost);
+    updateProduct(newQuantity, productname);
+  }
 }
 
-function runSearch() {
-  var questions = [
+function updateProduct(newQuantity, productname) {
+  connection.query("UPDATE products SET ? WHERE ?",
+  [
     {
-        message: "What is the product ID?",
-        type: "input",
-        name: "productId"
+      stock_quantity: newQuantity
     },
     {
-      message: "How many would you like to buy?",
-      type: "input",
-      name: "quantity"
-    }];
-  inquirer.prompt(questions);
-  console.log("And your answers are:", answers);
+      product_name: productname
+    }
+  ],
+  function(err) {
+    if (err) throw err;
+  });
+  purchaseQuestion2(productname);
+}
+
+function purchaseQuestion2(productname){
+  inquirer.prompt({
+    name: "anotherPurchase",
+    type: "confirm",
+    message: "Thank you for your purchase of " +productname + "! Would you like to make another purchase?",
+  }).then(function (action) {handleAnswer(action);})
+}
+
+function handleAnswer(action){
+  if (action.anotherPurchase){
+    promptQuestion();
   }
- 
+  if (!action.anotherPurchase){
+    console.log("Have a good day!")
+    connection.end();
+  }
+}
